bf94df40560e943a188ad2711ea37aed44c231f2
calinbaciu93-design <calinbaciu93@gmail.com>
2026-04-26 12:48:38 +0100

    Update birthday.js
    changed to finalise it to messages. couldn't do it before because I didn't have twilio paid version

--- birthday_bot/birthday.js
+++ birthday_bot/birthday.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const twilio = require('twilio');
+const fs = require("fs");
+const path = require("path");
+const twilio = require("twilio");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -9,44 +9,42 @@ const twilioNumber = process.env.TWILIO_NUMBER;
 const client = twilio(accountSid, authToken);
 
 function checkBirthday() {
-  const dataPath = path.join(__dirname, 'database.json');
-  const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
-                                          
-      const today = new Date();
-      const todayMonth = today.getMonth();
-      const todayDay = today.getDate();
+  const dataPath = path.join(__dirname, "database.json");
+  const data = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
 
-      let birthdayPeople = 0;
+  const today = new Date();
+  const todayMonth = today.getMonth();
+  const todayDay = today.getDate();
 
-      data.people.forEach((person) => {
-        const birthday = new Date(person.birthday);
+  let birthdayPeople = 0;
 
-        if (
-          birthday.getMonth() === todayMonth &&
-          birthday.getDate() === todayDay
-        ) {
-          birthdayPeople++;
-          console.log(`Happy Birthday, ${person.fullName}!`);
-          
-          client.calls.create({
-            to: person.phone,
-            from: twilioNumber,
-            twiml: `
-              <Response>
-                <Say voice="alice" language="en-US">
-                  Happy Birthday, ${person.fullName}! Have a good one!
-                </Say>
-              </Response>
-            `
+  data.people.forEach((person) => {
+    const birthday = new Date(person.birthday);
+
+    if (birthday.getMonth() === todayMonth && birthday.getDate() === todayDay) {
+      birthdayPeople++;
+      console.log(`Happy Birthday, ${person.fullName}!`);
+
+      client.messages
+        .create({
+          body: `Happy Birthday, ${person.fullName}! Have a good one!`,
+          from: twilioNumber,
+          to: person.phone,
         })
-          .then(call => console.log("Call initiated:", call.sid))
-          .catch(err => console.log("Call error:", err));
-        }
-      });
 
-      if (birthdayPeople === 0) {
-        console.log("No birthdays today.");
-      }
+        .then((message) => {
+          console.log(`Message sent: ${message.sid}`);
+        })
+
+        .catch((err) => {
+          console.error(`Error sending message: ${err}`);
+        });
+    }
+  });
+
+  if (birthdayPeople === 0) {
+    console.log("No birthdays today.");
+  }
 }
 
 checkBirthday();
